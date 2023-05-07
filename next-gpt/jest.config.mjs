7237bc3bc9b87f05215da09ff9d7e496e4967772
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-07 16:32:25 -0300

    [:gear:]  | create: config - babel (jest)

--- next-gpt/jest.config.mjs
+++ next-gpt/jest.config.mjs
@@ -7,10 +7,24 @@ const createJestConfig = nextJest({
 
 /** @type {import('jest').Config} */
 const config ={
+  roots: ["<rootDir>/src", "<rootDir>/test"],
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
     "^@/(.*)$": '<rootDir>/$1'
-  }
+  },
+  preset: 'ts-jest',
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  collectCoverageFrom: [
+    "<rootDir>/src/**/*.ts",
+    "<rootDir>/src/**/*.tsx",
+    "<rootDir>/test/**/*.ts"
+  ]
 };
 
 export default createJestConfig(config)
