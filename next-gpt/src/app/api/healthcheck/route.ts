2c25e212ff85b372f467db11a9bfde0059996db9
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-07 16:33:16 -0300

    [:sparkles:]  | update: route - healthcheck (api)

--- next-gpt/src/app/api/healthcheck/route.ts
+++ next-gpt/src/app/api/healthcheck/route.ts
@@ -1,5 +1,6 @@
+import { HEALTH_CHECK } from "@/config/response"
 import { NextResponse } from "next/server"
 
 export async function GET(request: Request) {
-  return NextResponse.json({ HELLO: "WORLD!" })
+  return NextResponse.json(HEALTH_CHECK)
 }
---
+++ next-gpt/src/config/index.ts
@@ -0,0 +1,1 @@
+export * as RESPONSE from "./response"
