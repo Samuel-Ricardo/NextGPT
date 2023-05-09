3c4a047fe422276ff6d9aba4812c621f0edaf54c
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-08 21:06:43 -0300

    [:gear:]  | update: config - ENV (envirioment)

---
+++ next-gpt/src/config/ENV.ts
@@ -0,0 +1,12 @@
+export const ENV = {
+  ...process.env,
+  PORT: process.env.PORT || 3000,
+  API: {
+    URL: process.env.API_URL || "http://localhost:3000/",
+  },
+  DOCKER: {
+    API: {
+      URL: process.env.DOCKER_API_URL || "http://app:3000/",
+    },
+  },
+}
