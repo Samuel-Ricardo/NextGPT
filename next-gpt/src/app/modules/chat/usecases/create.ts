d0d8b04b94665f667c01f57bbbc8e8494bc375c7
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-08 23:43:45 -0300

    [:sparkles:]  | create: use-case - Chat (module)

---
+++ next-gpt/src/app/modules/chat/usecases/create.ts
@@ -0,0 +1,17 @@
+import { ICreateChatDTO } from "../DTO"
+import { Chat } from "../entity"
+import { IChatRepository } from "../repository"
+
+export class CreateChatUseCase {
+  constructor(private repository: IChatRepository) {}
+
+  async execute(data: ICreateChatDTO): Promise<Chat> {
+    const chat = await this.repository.create(data)
+
+    if (!chat) {
+      throw new Error("Chat not found")
+    }
+
+    return chat
+  }
+}
