f37abc7b3908e2b46fba1954e2e5f256d64edcb5
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-08 23:26:10 -0300

    [:sparkles:]  | create: entity - Message (module)

---
+++ next-gpt/src/app/modules/message/entity/index.ts
@@ -0,0 +1,16 @@
+import { Chat } from "@modules/chat/entity"
+import { IMessage } from "../model"
+
+export class Message {
+  id?: string
+  chat_id!: string
+  content!: string
+  has_answered!: boolean
+  is_from_bot!: boolean
+  created_at!: Date
+  chat!: Chat
+
+  constructor(IMessage: IMessage) {
+    Object.assign(this, IMessage)
+  }
+}
