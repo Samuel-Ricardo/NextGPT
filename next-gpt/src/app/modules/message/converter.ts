956ea4c7276210c0904d453491ee9e3ac04dd3c4
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-08 22:46:59 -0300

    [:sparkles:]  | create: converter - message (modules)

--- next-gpt/src/app/modules/message/converter.ts
+++ next-gpt/src/app/modules/message/converter.ts
@@ -8,3 +8,14 @@ interface IMessageData {
   is_from_bot?: boolean
   created_at?: Date
 }
+
+export function message(data: IMessageData): IMessage {
+  return {
+    id: data.id,
+    chat_id: data.chat_id,
+    content: data.content ? data.content : "",
+    has_answered: data.has_aswered,
+    is_from_bot: data.is_from_bot,
+    created_at: data.created_at,
+  } as IMessage
+}
