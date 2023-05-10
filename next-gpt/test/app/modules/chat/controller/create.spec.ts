88555418885562cc61aabc045ede41262d804b95
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-10 10:58:59 -0300

    [:white_check_mark:]  | create: unit test - controller (chat)

--- next-gpt/test/app/modules/chat/controller/create.spec.ts
+++ next-gpt/test/app/modules/chat/controller/create.spec.ts
@@ -1,6 +1,7 @@
 import { ChatController } from "@/app/modules/chat/controller"
 import { ChatService } from "@modules/chat/service"
-import { resetMocks } from "../../../../utils/mock"
+import { VALID_CHAT } from "@/config/const"
+import { resetMocks } from "@test/utils/mock"
 
 describe("Controller => Chat", () => {
   jest.mock("../../../../../src/app/modules/chat/service/index")
@@ -16,5 +17,17 @@ describe("Controller => Chat", () => {
     controller = new ChatController(service)
   })
 
-  it("Should create a Chat successfully", async () => {})
+  it("Should create a Chat successfully", async () => {
+    jest.spyOn(service, "createChat").mockResolvedValue({
+      ...VALID_CHAT,
+      remote_chat_id: undefined,
+    })
+
+    await expect(
+      controller.create({ message: "Hello World" })
+    ).resolves.toStrictEqual({
+      ...VALID_CHAT,
+      remote_chat_id: undefined,
+    })
+  })
 })
