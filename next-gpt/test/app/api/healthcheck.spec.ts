fb379fe5041b128647c98bb40b9293850eda9b12
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-09 22:39:53 -0300

    [:white_check_mark:]  | update: test - healthcheck api

--- next-gpt/test/app/api/healthcheck.spec.ts
+++ next-gpt/test/app/api/healthcheck.spec.ts
@@ -1,9 +1,9 @@
 import "isomorphic-fetch"
-import { ENV, RESPONSE } from "@/config"
+import { RESPONSE, ROUTES } from "@/config"
 
 describe("healthcheck", () => {
   it("API should be ok", async () => {
-    const response = await fetch(`${ENV.API.URL}api/healthcheck`)
+    const response = await fetch(ROUTES.HEALTH_CHECK)
     const data = await response.json()
 
     expect(response.status).toBe(200)
