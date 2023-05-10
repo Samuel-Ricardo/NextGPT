5f79a9ef52fbb7a57203bc90287a1971d05604f4
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-09 23:21:40 -0300

    [:recycle:]  | refact: import - type (converter)

--- next-gpt/src/app/modules/message/converter.ts
+++ next-gpt/src/app/modules/message/converter.ts
@@ -1,13 +1,5 @@
+import { IMessageData } from "@Types"
 import { IMessage } from "./model"
-
-interface IMessageData {
-  id?: string
-  chat_id?: string
-  content?: string
-  has_aswered?: boolean
-  is_from_bot?: boolean
-  created_at?: Date
-}
 
 export function message(data: IMessageData): IMessage {
   return {
