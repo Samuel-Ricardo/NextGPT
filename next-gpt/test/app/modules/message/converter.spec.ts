9a58efd3fefd989d7be14e84a8e06e4e08674583
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-09 23:16:22 -0300

    [:white_check_mark:]  | create: unit test - convert message (converter)

---
+++ next-gpt/test/app/modules/message/converter.spec.ts
@@ -0,0 +1,11 @@
+import { IMessage } from "@/app/modules/message/model"
+import { VALID_IMESSAGE_WITHOUT_CHAT, VALID_MESSAGE_DATA } from "@/config/const"
+import { message, messages } from "@modules/message/converter"
+
+describe("Message Converter", () => {
+  it("should convert message data to Message Interface", () => {
+    expect(message(VALID_MESSAGE_DATA)).toStrictEqual(
+      VALID_IMESSAGE_WITHOUT_CHAT
+    )
+  })
+})
