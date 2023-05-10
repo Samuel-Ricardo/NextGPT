da69653c81f45681e8370874406eab0f32756ecb
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-10 11:48:16 -0300

    [:white_check_mark:]  | create: test - create chat (api)

---
+++ next-gpt/test/app/api/chat/route.spec.ts
@@ -0,0 +1,19 @@
+import { ROUTES } from "@/config"
+import { Chat } from "@prisma/client"
+import "isomorphic-fetch"
+
+describe("[API] - route: /chat", () => {
+  it("POST /chat", async () => {
+    const response = await fetch(ROUTES.CHAT, {
+      method: "POST",
+      body: JSON.stringify({ message: "Hello World" }),
+    })
+
+    const created: { chat: Chat } = await response.json()
+
+    console.log({ created })
+
+    expect(response.status).toBe(200)
+    expect(created.chat).toHaveProperty("id")
+  })
+})
--- next-gpt/test/app/modules/chat/factory.ts
+++ next-gpt/test/app/modules/chat/factory.ts
@@ -2,7 +2,7 @@ import { ChatController } from "@modules/chat/controller"
 import { chatFactory } from "@modules/chat/factory"
 
 describe("Chat Factory", () => {
-  it("Should create a Chat controller instance", (_) => {
+  it("Should create a Chat controller instance", () => {
     expect(chatFactory()).toBeInstanceOf(ChatController)
   })
 })
