b7b0f547cea1c9b23b8010944a074ac70cc8f721
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-11 11:25:26 -0300

    [:sparkles:]  | create: fun - selectMessage (service)

--- next-gpt/src/app/modules/chat/service/index.ts
+++ next-gpt/src/app/modules/chat/service/index.ts
@@ -1,11 +1,15 @@
-import { ICreateChatDTO } from "../DTO"
+import { ICreateChatDTO, IGetMessagesDTO } from "../DTO"
+import { AppendMessageUseCase } from "../usecases/append_message"
 import { CreateChatUseCase } from "../usecases/create"
 import { selectAllChatUseCase } from "../usecases/select_all"
+import { SelectMessageUseCase } from "../usecases/select_message"
 
 export class ChatService {
   constructor(
     private create: CreateChatUseCase,
-    private selectAll: selectAllChatUseCase
+    private selectAll: selectAllChatUseCase,
+    private select: SelectMessageUseCase,
+    private addMessage: AppendMessageUseCase
   ) {}
 
   async createChat(data: ICreateChatDTO) {
@@ -14,5 +18,9 @@ export class ChatService {
 
   async selectAllChats() {
     return await this.selectAll.execute()
+  }
+
+  async selectMessage(data: IGetMessagesDTO) {
+    return await this.select.execute(data)
   }
 }
