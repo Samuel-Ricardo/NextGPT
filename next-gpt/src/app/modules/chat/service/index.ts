0154d57142a59cb1dd9179bff48f819f45370858
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-11 10:10:09 -0300

    [:sparkles:]  | update: usecase - select all (chat)

--- next-gpt/src/app/modules/chat/service/index.ts
+++ next-gpt/src/app/modules/chat/service/index.ts
@@ -1,10 +1,18 @@
 import { ICreateChatDTO } from "../DTO"
 import { CreateChatUseCase } from "../usecases/create"
+import { selectAllChatUseCase } from "../usecases/select_all"
 
 export class ChatService {
-  constructor(private create: CreateChatUseCase) {}
+  constructor(
+    private create: CreateChatUseCase,
+    private selectAll: selectAllChatUseCase
+  ) {}
 
   async createChat(data: ICreateChatDTO) {
     return await this.create.execute(data)
+  }
+
+  async selectAllChats() {
+    return await this.selectAll.execute()
   }
 }
