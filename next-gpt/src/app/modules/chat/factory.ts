7073d23267dd5e7182e2fcf2dfd3e591b89b4876
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-11 11:36:16 -0300

    [:sparkles:]  | update: factory (chat)

--- next-gpt/src/app/modules/chat/factory.ts
+++ next-gpt/src/app/modules/chat/factory.ts
@@ -4,14 +4,18 @@ import { ChatService } from "./service"
 import { CreateChatUseCase } from "./usecases/create"
 import { prisma } from "@prisma"
 import { selectAllChatUseCase } from "./usecases/select_all"
+import { AppendMessageUseCase } from "./usecases/append_message"
+import { SelectMessageUseCase } from "./usecases/select_message"
 
 export async function chatFactory() {
   const repository = new ChatPrismaRepository(prisma)
 
   const create = new CreateChatUseCase(repository)
   const selectAll = new selectAllChatUseCase(repository)
+  const select = new SelectMessageUseCase(repository)
+  const append = new AppendMessageUseCase(repository)
 
-  const service = new ChatService(create, selectAll)
+  const service = new ChatService(create, selectAll, select, append)
   const controller = new ChatController(service)
 
   return controller
