b72af6e8f7b93c5ad5bf0f60964853a43fcfee78
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-11 11:08:23 -0300

    [:card_file_box:]  | update: repositor - interface (chat)

--- next-gpt/src/app/modules/chat/repository/index.ts
+++ next-gpt/src/app/modules/chat/repository/index.ts
@@ -5,6 +5,6 @@ import { Chat } from "../entity"
 export interface IChatRepository {
   create(data: ICreateChatDTO): Promise<Chat>
   selectAll(): Promise<Chat[]>
-  getMessages(data: IGetMessagesDTO): Promise<Message[]>
+  selectMessage(data: IGetMessagesDTO): Promise<Message[]>
   addMessageTo(data: IAddMessageDTO): Promise<Message>
 }
