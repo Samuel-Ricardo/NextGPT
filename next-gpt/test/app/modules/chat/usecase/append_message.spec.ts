c9a64cbf47237639ee61d56504606277f3d2a70f
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-12 11:17:42 -0300

    [:white_check_mark:]  | create: unit test - add message to chat (usecase)

--- next-gpt/test/app/modules/chat/usecase/append_message.spec.ts
+++ next-gpt/test/app/modules/chat/usecase/append_message.spec.ts
@@ -1,6 +1,8 @@
 import { AppendMessageUseCase } from "@/app/modules/chat/usecases/append_message"
 import { prisma_mock } from "@/app/prisma/test/mock"
 import { ChatPrismaRepository } from "@/app/repository/prisma/chat"
+import { VALID_CHAT, VALID_IMESSAGE_WITHOUT_CHAT } from "@/config/const"
+import { Chat, Message } from "@prisma/client"
 import { resetMocks } from "@test/utils/mock"
 
 describe("[CHAT] - use-case => add: message to: chat", () => {
@@ -20,5 +22,21 @@ describe("[CHAT] - use-case => add: message to: chat", () => {
     append = new AppendMessageUseCase(repository)
   })
 
-  it("Should add message to a chat successfully", async () => {})
+  it("Should add message to a chat successfully", async () => {
+    prisma_mock.chat.findUniqueOrThrow.mockResolvedValue(VALID_CHAT as Chat)
+    prisma_mock.message.create.mockResolvedValue(
+      VALID_IMESSAGE_WITHOUT_CHAT as Message
+    )
+
+    console.log({
+      RESULT: await append.execute({
+        chat_id: VALID_CHAT.id!,
+        message: "Hello World",
+      }),
+    })
+
+    await expect(
+      append.execute({ chat_id: VALID_CHAT.id!, message: "Hello World" })
+    ).resolves.toEqual(VALID_IMESSAGE_WITHOUT_CHAT)
+  })
 })
