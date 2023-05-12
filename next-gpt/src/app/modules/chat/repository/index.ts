a72a7f54383d74c54ce07e72565b1976d6f10ba9
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-12 10:42:58 -0300

    [:sparkles:]  | create: interface - generic (repository)

--- next-gpt/src/app/modules/chat/repository/index.ts
+++ next-gpt/src/app/modules/chat/repository/index.ts
@@ -1,8 +1,9 @@
 import { Message } from "@modules/message/entity"
 import { IAddMessageDTO, ICreateChatDTO, IGetMessagesDTO } from "../DTO"
 import { Chat } from "../entity"
+import { IRepository } from "@repository/generic"
 
-export interface IChatRepository {
+export interface IChatRepository extends IRepository<Chat> {
   create(data: ICreateChatDTO): Promise<Chat>
   selectAll(): Promise<Chat[]>
   selectMessage(data: IGetMessagesDTO): Promise<Message[]>
