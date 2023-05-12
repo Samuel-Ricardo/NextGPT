bc9c1f30e46c9939d71f9410963594173af35cc0
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-12 08:03:35 -0300

    [:gear:]  | create: route - chat/[chatId]/messages (config)

--- next-gpt/src/config/routes.ts
+++ next-gpt/src/config/routes.ts
@@ -4,3 +4,5 @@ export const BASE = `${ENV.API.URL()}api`
 
 export const HEALTH_CHECK = `${BASE}/healthcheck`
 export const CHAT = `${BASE}/chat`
+
+export const CHAT_MESSAGES = (chatId: string) => `${CHAT}/${chatId}/messages`
