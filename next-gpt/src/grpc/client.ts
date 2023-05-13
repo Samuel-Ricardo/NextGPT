c65557cb9c6e3f5d39c1caa43f918d35463b8e79
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-13 13:43:54 -0300

    [:recycle:]  | refact: host (Grpc)

--- next-gpt/src/grpc/client.ts
+++ next-gpt/src/grpc/client.ts
@@ -2,6 +2,7 @@ import * as protoLoader from "@grpc/proto-loader"
 import * as grpc from "@grpc/grpc-js"
 import path from "path"
 import { ProtoGrpcType } from "./rpc/chat"
+import { ENV } from "@/config"
 
 const packageDefinition = protoLoader.loadSync(
   path.resolve(process.cwd(), "proto", "chat.proto")
@@ -11,7 +12,7 @@ const proto = grpc.loadPackageDefinition(
 ) as unknown as ProtoGrpcType
 
 export const chatClient = new proto.pb.ChatService(
-  "host.docker.internal:50052",
+  ENV.GRCP.SERVICE.URL(),
   grpc.credentials.createInsecure()
 )
 
