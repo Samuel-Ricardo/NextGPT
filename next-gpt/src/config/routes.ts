897c7187b830f7f24bdf7b462770482269798fdb
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-13 14:27:45 -0300

    [:gear:]  | update: config - routes => Chat Evetns (config)

--- next-gpt/src/config/routes.ts
+++ next-gpt/src/config/routes.ts
@@ -6,3 +6,6 @@ export const HEALTH_CHECK = `${BASE}/healthcheck`
 export const CHAT = `${BASE}/chat`
 
 export const CHAT_MESSAGES = (chatId: string) => `${CHAT}/${chatId}/messages`
+
+export const MESSAGES_EVENTS = (messageId: string) =>
+  `${BASE}/messages/${messageId}/events`
