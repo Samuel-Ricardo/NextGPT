8716d68d92fb3b44aad59c975518297973da57b5
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-14 12:15:16 -0300

    [:sparkles:]  | create:  fun - create (/prisma/repository/message)

--- next-gpt/src/app/repository/prisma/message.ts
+++ next-gpt/src/app/repository/prisma/message.ts
@@ -1,5 +1,16 @@
+import { message } from "@modules/message/converter"
+import { ICreateMessageDTO } from "@modules/message/DTO"
+import { Message } from "@modules/message/entity"
 import { IMessageRepository } from "@modules/message/repository"
+import { PrismaClient } from "@prisma/client"
 
-class PrismaMessageRepository implements IMessageRepository {}
+class PrismaMessageRepository implements IMessageRepository {
+  constructor(private prisma: PrismaClient) {}
+
+  async create(data: ICreateMessageDTO): Promise<Message> {
+    const result = await this.prisma.message.create({ data })
+    return message(result)
+  }
+}
 
 export { PrismaMessageRepository }
