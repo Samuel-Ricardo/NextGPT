7a4bb451dba6a06a3f66bd802b7526d62ee9670e
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-14 12:24:13 -0300

    [:sparkles:]  | create:  fun - update (/prisma/repository/message)

--- next-gpt/src/app/repository/prisma/message.ts
+++ next-gpt/src/app/repository/prisma/message.ts
@@ -1,5 +1,5 @@
 import { message } from "@modules/message/converter"
-import { ICreateMessageDTO } from "@modules/message/DTO"
+import { ICreateMessageDTO, IUpdateMessageDTO } from "@modules/message/DTO"
 import { Message } from "@modules/message/entity"
 import { IMessageRepository } from "@modules/message/repository"
 import { PrismaClient } from "@prisma/client"
@@ -9,6 +9,22 @@ class PrismaMessageRepository implements IMessageRepository {
 
   async create(data: ICreateMessageDTO): Promise<Message> {
     const result = await this.prisma.message.create({ data })
+    return message(result)
+  }
+
+  async update(data: IUpdateMessageDTO): Promise<Message> {
+    const result = await this.prisma.message.update({
+      where: { id: data.id },
+      data: {
+        id: data.id,
+        chat_id: data.chat_id,
+        has_answered: data.has_answered,
+        content: data.content,
+        is_from_bot: data.is_from_bot,
+        created_at: data.created_at,
+      },
+    })
+
     return message(result)
   }
 }
