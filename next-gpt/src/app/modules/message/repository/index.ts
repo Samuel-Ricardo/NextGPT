dd9946a39b0e02231e9f2ebb1ecc7a3cae2e3915
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-14 13:35:10 -0300

    [:sparkles:]  | update: interface - repository (message)

--- next-gpt/src/app/modules/message/repository/index.ts
+++ next-gpt/src/app/modules/message/repository/index.ts
@@ -1,3 +1,4 @@
+import { Chat } from "@modules/chat/entity"
 import { ICreateMessageDTO, IUpdateMessageDTO } from "../DTO"
 import { Message } from "../entity"
 
@@ -5,4 +6,5 @@ export interface IMessageRepository {
   create(data: ICreateMessageDTO): Promise<Message>
   update(data: IUpdateMessageDTO): Promise<Message>
   selectById(id: string): Promise<Message>
+  selectByIdWithChat(id: string): Promise<Chat>
 }
