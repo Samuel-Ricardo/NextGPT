81cb822bd2ec1f5ebe621830b0f9aee2646d33b0
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-14 11:59:33 -0300

    [:white_check_mark:]  | update: test - GRPC chat client connection

---
+++ next-gpt/test/app/api/messages/events/route.spec.ts
@@ -0,0 +1,9 @@
+import { MESSAGES_EVENTS } from "@/config/routes"
+import "isomorphic-fetch"
+
+describe("[API] - Message Events", () => {
+  it("Should connect sucssessfully", async () => {
+    // const response = await fetch(`${MESSAGES_EVENTS("1")}`)
+    // expect(response.status).toBe(200)
+  })
+})
