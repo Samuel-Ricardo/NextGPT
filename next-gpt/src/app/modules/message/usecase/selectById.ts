e70a594d6c798e9a1d21ac39e26ba84476341022
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-15 09:54:11 -0300

    [:sparkles:]  | create: usecase - selectById (message)

--- next-gpt/src/app/modules/message/usecase/selectById.ts
+++ next-gpt/src/app/modules/message/usecase/selectById.ts
@@ -2,4 +2,8 @@ import { PrismaMessageRepository } from "@repository/prisma/message"
 
 export class SelectMessageByIdUseCase {
   constructor(private prisma: PrismaMessageRepository) {}
+
+  async execute(id: string) {
+    return this.prisma.selectById(id)
+  }
 }
