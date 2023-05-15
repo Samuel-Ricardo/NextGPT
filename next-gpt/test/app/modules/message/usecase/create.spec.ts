041afe63bebbe9e1e2261e947015c29d04de8099
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-15 11:19:27 -0300

    [:white_check_mark:]  | create: test - select by id with message (usecase)

--- next-gpt/test/app/modules/message/usecase/create.spec.ts
+++ next-gpt/test/app/modules/message/usecase/create.spec.ts
@@ -1,12 +1,15 @@
-import { CreateMessageUseCase } from "@/app/modules/message/usecase/create"
+import { CreateMessageUseCase } from "@modules/message/usecase/create"
 import { PrismaMessageRepository } from "@repository/prisma/message"
 import { mockMessageRepository } from "@test/mock/repository"
+import { resetMocks } from "@test/utils/mock"
 
 describe("[USECASE] => Create Message", () => {
   let repository: jest.Mocked<PrismaMessageRepository>
   let create: CreateMessageUseCase
 
   beforeEach(() => {
+    resetMocks()
+
     repository = mockMessageRepository()
     create = new CreateMessageUseCase(repository)
   })
