8a35472e87ad24879edeff1741b444b6aa218d91
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-15 15:23:43 -0300

    [:white_check_mark:]  | setup: test suit - controller (message)

--- next-gpt/test/app/modules/message/controller/controller.ts
+++ next-gpt/test/app/modules/message/controller/controller.ts
@@ -1,4 +1,18 @@
+import { MessageController } from "@/app/modules/message/controller"
+import { mockMessageService } from "@test/mock/service/message"
+import { resetMocks } from "@test/utils/mock"
+
 describe("[Controller] => Message", () => {
+  let service = mockMessageService()
+  let controller: MessageController
+
+  beforeEach(() => {
+    resetMocks()
+
+    service = mockMessageService()
+    controller = new MessageController(service)
+  })
+
   it("should be defined", () => {
     expect(true).toBeTruthy()
   })
