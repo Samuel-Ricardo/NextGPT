5654317b1c0d72fcb5103afcee71ed49e9d31f76
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-15 11:25:20 -0300

    [:white_check_mark:]  | create: test - create message (usecase)

--- next-gpt/test/app/modules/message/usecase/create.spec.ts
+++ next-gpt/test/app/modules/message/usecase/create.spec.ts
@@ -1,3 +1,5 @@
+import { message } from "@modules/message/converter"
+import { VALID_IMESSAGE_WITHOUT_CHAT } from "@/config/const"
 import { CreateMessageUseCase } from "@modules/message/usecase/create"
 import { PrismaMessageRepository } from "@repository/prisma/message"
 import { mockMessageRepository } from "@test/mock/repository"
@@ -14,5 +16,12 @@ describe("[USECASE] => Create Message", () => {
     create = new CreateMessageUseCase(repository)
   })
 
-  it("should be able to create a message", async () => {})
+  it("should be able to create a message", async () => {
+    repository.create.mockResolvedValue(message(VALID_IMESSAGE_WITHOUT_CHAT))
+
+    const _message = await create.execute(VALID_IMESSAGE_WITHOUT_CHAT)
+
+    expect(_message).toEqual(VALID_IMESSAGE_WITHOUT_CHAT)
+    expect(repository.create).toHaveBeenCalledTimes(1)
+  })
 })
