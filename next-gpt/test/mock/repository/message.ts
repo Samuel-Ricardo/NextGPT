852af0049a08eb75f1fac125ede2ad9435a31ab8
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-15 10:49:54 -0300

    [:adhesive_bandage:]  | fix: import - repository (message)

--- next-gpt/test/mock/repository/message.ts
+++ next-gpt/test/mock/repository/message.ts
@@ -1,7 +1,7 @@
 import { prisma_mock } from "@prisma/test/mock"
 import { PrismaMessageRepository } from "@repository/prisma/message"
 
-jest.mock("../../src/app/repository/prisma/message")
+jest.mock("../../../src/app/repository/prisma/message")
 
 export const messageRepositoryMock =
   PrismaMessageRepository as jest.Mock<PrismaMessageRepository>
