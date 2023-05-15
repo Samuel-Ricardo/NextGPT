aa26fce2cea63f3e6fe4b18c43034e8d582cb16e
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-15 11:48:44 -0300

    [:clown_face:]  | create:  mock - select by id with chat (usecase)

---
+++ next-gpt/test/mock/usecase/message/selectByIdWithChat.ts
@@ -0,0 +1,10 @@
+import { SelectMessageByIdWithChatUseCase } from "@modules/message/usecase"
+
+jest.mock("../../../../src/app/modules/message/usecase/selectByIdWithChat")
+
+const selectByIdWithChatMock =
+  SelectMessageByIdWithChatUseCase as jest.Mock<SelectMessageByIdWithChatUseCase>
+const mockSelectByIdWithChatUseCase = () =>
+  new selectByIdWithChatMock() as jest.Mocked<SelectMessageByIdWithChatUseCase>
+
+export { selectByIdWithChatMock, mockSelectByIdWithChatUseCase }
