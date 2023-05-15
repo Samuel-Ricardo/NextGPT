c8fb1a24927d7dd016c11e076eedd6e71959ad69
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-15 14:25:07 -0300

    [:white_check_mark:]  | create: unit test - service [select by id] (message)

--- next-gpt/test/app/modules/message/service/index.spec.ts
+++ next-gpt/test/app/modules/message/service/index.spec.ts
@@ -32,4 +32,21 @@ describe("[service] => Message", () => {
       VALID_IMESSAGE_WITHOUT_CHAT
     )
   })
+
+  it("should be able to select a Message with id", async () => {
+    selectById.execute.mockResolvedValue(message(VALID_IMESSAGE_WITHOUT_CHAT))
+
+    await expect(
+      selectById.execute(VALID_IMESSAGE_WITHOUT_CHAT.id)
+    ).resolves.toEqual(VALID_IMESSAGE_WITHOUT_CHAT)
+
+    expect(selectById.execute).toHaveBeenCalledWith(
+      VALID_IMESSAGE_WITHOUT_CHAT.id
+    )
+
+    const result = await selectById.execute(VALID_IMESSAGE_WITHOUT_CHAT.id)
+    expect(result).not.toHaveProperty("chat")
+
+    expect(selectById.execute).toHaveBeenCalledTimes(2)
+  })
 })
