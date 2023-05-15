64afcc0895ee9fdd939e72ed4ffc83349f667b0b
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-15 14:49:17 -0300

    [:sparkles:]  | create: service - message (module)

--- next-gpt/src/app/modules/message/service/index.ts
+++ next-gpt/src/app/modules/message/service/index.ts
@@ -1,3 +1,6 @@
+import { Chat } from "@modules/chat/entity"
+import { ICreateMessageDTO } from "../DTO"
+import { Message } from "../entity"
 import {
   SelectMessageByIdUseCase,
   SelectMessageByIdWithChatUseCase,
@@ -10,4 +13,16 @@ export class MessageService {
     private readonly selectById: SelectMessageByIdUseCase,
     private readonly selectByIdWithChat: SelectMessageByIdWithChatUseCase
   ) {}
+
+  async createMessage(data: ICreateMessageDTO): Promise<Message> {
+    return await this.create.execute(data)
+  }
+
+  async selectMessageById(id: string): Promise<Message> {
+    return await this.selectById.execute(id)
+  }
+
+  async selectMessageByIdWithChat(id: string): Promise<Chat> {
+    return await this.selectByIdWithChat.execute(id)
+  }
 }
