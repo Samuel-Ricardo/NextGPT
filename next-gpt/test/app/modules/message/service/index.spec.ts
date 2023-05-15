aaa974e4cfa8f490d707534686d27242325ca718
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-15 14:13:55 -0300

    [:white_check_mark:]  | setup: test suit - service (message)

--- next-gpt/test/app/modules/message/service/index.spec.ts
+++ next-gpt/test/app/modules/message/service/index.spec.ts
@@ -1,4 +1,31 @@
+import { MessageService } from "@/app/modules/message/service"
+import {
+  createMessageMock,
+  mockCreateMessageUseCase,
+  mockSelectByIdWithChatUseCase,
+  mockSelectMessageById,
+  selectByIdMock,
+  selectByIdWithChatMock,
+} from "@test/mock/usecase/message"
+import { resetMocks } from "@test/utils/mock"
+
 describe("[service] => Message", () => {
+  let create = mockCreateMessageUseCase()
+  let selectById = mockSelectMessageById()
+  let selectByIdWithChat = mockSelectByIdWithChatUseCase()
+
+  let service
+
+  beforeEach(() => {
+    resetMocks()
+
+    create = mockCreateMessageUseCase()
+    selectById = mockSelectMessageById()
+    selectByIdWithChat = mockSelectByIdWithChatUseCase()
+
+    service = new MessageService(create, selectById, selectByIdWithChat)
+  })
+
   it("should be able to create a Message", () => {
     expect(true).toBe(true)
   })
