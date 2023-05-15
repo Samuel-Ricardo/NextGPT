b608465c6057816e106933d8907a888acb25dc15
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-15 10:28:15 -0300

    [:white_check_mark:]  | create: test suit - select by id usecases

---
+++ next-gpt/test/app/modules/message/usecase/selectById.spec.ts
@@ -0,0 +1,9 @@
+import {
+  SelectMessageByIdUseCase,
+  SelectMessageByIdWithChatUseCase,
+} from "@modules/message/usecase"
+import { mockMessageRepository } from "@test/mock/repository"
+
+describe("Select Message By ID usecases", () => {
+  it("should select message by id", async () => {})
+})
