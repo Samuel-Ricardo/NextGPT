72ea12e5130ff9354274896ea3d5e49a0debee9e
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-15 09:48:31 -0300

    [:sparkles:]  | create: converter - chat (modules)

---
+++ next-gpt/src/app/modules/chat/converter.ts
@@ -0,0 +1,12 @@
+import { messages } from "../message/converter"
+import { IChatConverterDTO } from "./DTO"
+import { Chat } from "./entity"
+
+export function chat(data: IChatConverterDTO): Chat {
+  return Chat.from({
+    id: data.id,
+    messages: data.messages ? messages(data.messages) : [],
+    remote_chat_id: data.remote_chat_id,
+    created_at: data.created_at!,
+  })
+}
