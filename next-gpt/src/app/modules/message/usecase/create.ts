bbfa5210eea20929221fd624e2be022eef2375cc
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-15 11:11:18 -0300

    [:sparkles:]  | update: usecase - create  (message)

--- next-gpt/src/app/modules/message/usecase/create.ts
+++ next-gpt/src/app/modules/message/usecase/create.ts
@@ -1,5 +1,13 @@
 import { PrismaMessageRepository } from "@repository/prisma/message"
+import { Message } from "../entity"
+import { message } from "../converter"
+import { ICreateMessageDTO } from "../DTO"
 
 export class CreateMessageUseCase {
-  constructor(private readonly prisma: PrismaMessageRepository) {}
+  constructor(private readonly repository: PrismaMessageRepository) {}
+
+  async execute(data: ICreateMessageDTO): Promise<Message> {
+    const result = await this.repository.create(data)
+    return message(result)
+  }
 }
