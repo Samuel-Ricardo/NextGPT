37410eca39e8675c152c024bebb8f08690d8b8b2
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-16 11:10:18 -0300

    [:white_check_mark:]  | create: test -  authCheck => is valid (user)

--- next-gpt/test/app/modules/user/usecase/authCheck.spec.ts
+++ next-gpt/test/app/modules/user/usecase/authCheck.spec.ts
@@ -1,5 +1,6 @@
 import { AuthCheckUseCase } from "@/app/modules/user/usecase"
 import { resetMocks } from "@test/utils/mock"
+import { JWT } from "next-auth/jwt"
 
 describe("[USECASE] - auth check (user)", () => {
   let check: AuthCheckUseCase
@@ -8,5 +9,8 @@ describe("[USECASE] - auth check (user)", () => {
     check = new AuthCheckUseCase()
   })
 
-  it("Should return [true] if token is valid", async () => {})
+  it("Should return [true] if token is valid", async () => {
+    const isValid = check.execute({ sub: "123" } as JWT)
+    expect(isValid).toBe(true)
+  })
 })
