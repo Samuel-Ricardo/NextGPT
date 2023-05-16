4fdf55eb3facc302738ff5ee571b3d366eebcdef
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-16 11:40:24 -0300

    [:clown_face:]  | create:  mock - use case => auth check (user)

---
+++ next-gpt/test/mock/usecase/user/auth_check.ts
@@ -0,0 +1,8 @@
+import { AuthCheckUseCase } from "@modules/user/usecase"
+
+jest.mock("../../../../src/app/modules/user/usecase/auth_check")
+
+export const authCheckMock = AuthCheckUseCase as jest.Mock<AuthCheckUseCase>
+
+export const mockAuthCheckUseCase = () =>
+  new authCheckMock() as jest.Mocked<AuthCheckUseCase>
