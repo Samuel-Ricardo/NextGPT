21bee26c607c5366c58afbecb5a082e9db0fee46
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-16 10:59:48 -0300

    [:white_check_mark:]  | setup: test suit - authCheck (user)

--- next-gpt/test/app/modules/user/usecase/authCheck.spec.ts
+++ next-gpt/test/app/modules/user/usecase/authCheck.spec.ts
@@ -1,3 +1,12 @@
+import { AuthCheckUseCase } from "@/app/modules/user/usecase"
+import { resetMocks } from "@test/utils/mock"
+
 describe("[USECASE] - auth check (user)", () => {
+  let check: AuthCheckUseCase
+
+  beforeEach(() => {
+    check = new AuthCheckUseCase()
+  })
+
   it("Should return [true] if token is valid", async () => {})
 })
