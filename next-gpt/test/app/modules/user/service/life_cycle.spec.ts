3bb62ce008a461ad8a36d28f1732c03b690bed29
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-16 11:50:00 -0300

    [:white_check_mark:]  | setup: test suit - service => life cycle (user)

--- next-gpt/test/app/modules/user/service/life_cycle.spec.ts
+++ next-gpt/test/app/modules/user/service/life_cycle.spec.ts
@@ -1,4 +1,25 @@
+import { UserLifeCycleService } from "@/app/modules/user/service/life_cycle"
+import {
+  mockAuthCheckUseCase,
+  mockUserNotFoundUseCase,
+} from "@test/mock/usecase"
+import { resetMocks } from "@test/utils/mock"
+
 describe("[SERVICE] - life cycle => user", () => {
+  let notFound = mockUserNotFoundUseCase()
+  let authCheck = mockAuthCheckUseCase()
+
+  let life_cycle: UserLifeCycleService
+
+  beforeEach(() => {
+    resetMocks()
+
+    notFound = mockUserNotFoundUseCase()
+    authCheck = mockAuthCheckUseCase()
+
+    life_cycle = new UserLifeCycleService(authCheck, notFound)
+  })
+
   it("should be valid user", () => {
     expect(true).toBe(true)
   })
