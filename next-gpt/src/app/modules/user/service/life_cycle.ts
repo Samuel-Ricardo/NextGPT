b538707ab167b24398a74d0d5bb1b398aa5155b0
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-15 21:56:17 -0300

    [:sparkles:]  | create: service - life cycle (user)

--- next-gpt/src/app/modules/user/service/life_cycle.ts
+++ next-gpt/src/app/modules/user/service/life_cycle.ts
@@ -1,3 +1,5 @@
+import { UnauthenticatedError } from "@/config/errors"
+import { IsValidUserDTO } from "../DTO"
 import { AuthCheckUseCase, UserNotFoundUseCase } from "../usecase"
 
 export class UserLifeCycleService {
@@ -5,4 +7,15 @@ export class UserLifeCycleService {
     private readonly authCheck: AuthCheckUseCase,
     private readonly notFound: UserNotFoundUseCase
   ) {}
+
+  async isValidUser({ token, user_id }: IsValidUserDTO) {
+    if (!this.authCheck.execute(token))
+      return { result: false, reason: new UnauthenticatedError() }
+
+    const reason = this.notFound.execute({ token: token!, user_id })
+
+    if (reason) return { result: false, reason }
+
+    return { result: true }
+  }
 }
