eadc5fc7a0185ab7ff813b0f096d3200cfcf274c
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-16 11:26:55 -0300

    [:white_check_mark:]  | setup: test suit - notFound (user)

--- next-gpt/test/app/modules/user/usecase/notFound.spec.ts
+++ next-gpt/test/app/modules/user/usecase/notFound.spec.ts
@@ -1,4 +1,12 @@
+import { UserNotFoundUseCase } from "@/app/modules/user/usecase"
+
 describe("[USECASE] Not Found => user", () => {
+  let notFound: UserNotFoundUseCase
+
+  beforeEach(() => {
+    notFound = new UserNotFoundUseCase()
+  })
+
   it("should undefined if user found ", async () => {
     expect(true).toBe(true)
   })
