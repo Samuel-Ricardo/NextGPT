71c2559afb2a7ef8ddb315c99a27c15147c00f23
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-15 22:04:06 -0300

    [:label:]  | update: model - chat

--- next-gpt/src/app/modules/chat/model.d.ts
+++ next-gpt/src/app/modules/chat/model.d.ts
@@ -2,6 +2,7 @@ import { IMessage } from "../message/model"
 
 export interface IChat {
   id?: string
+  user_id: string
   remote_chat_id?: string
   created_at: Date
   messages: IMessage[] | null
