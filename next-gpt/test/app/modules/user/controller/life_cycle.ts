e912688b8c935d39507d21c7920257e2bdcf6cd4
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-16 21:57:48 -0300

    [:white_check_mark:]  | setup: test suit - controller => life cycle (user)

--- next-gpt/test/app/modules/user/controller/life_cycle.ts
+++ next-gpt/test/app/modules/user/controller/life_cycle.ts
@@ -1,3 +1,17 @@
+import { UserLifeCycleController } from "@modules/user/controller/lyfe_cycle"
+import { mockLifeCycleService } from "@test/mock/service/user/life_cycle"
+import { resetMocks } from "@test/utils/mock"
+
 describe("[Controller] - Life Cycle => User", () => {
+  let service = mockLifeCycleService()
+  let controller: UserLifeCycleController
+
+  beforeEach(() => {
+    resetMocks()
+
+    service = mockLifeCycleService()
+    controller = new UserLifeCycleController(service)
+  })
+
   it("isValid", async () => {})
 })
