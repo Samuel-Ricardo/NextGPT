b538f568910f67165aad9c1ac3148a25eb8ca467
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-19 11:05:49 -0300

    [:label:]  | update: DTO - create (message)

--- next-gpt/src/app/modules/message/DTO/create.d.ts
+++ next-gpt/src/app/modules/message/DTO/create.d.ts
@@ -3,4 +3,9 @@ export interface ICreateMessageDTO {
   chat_id: string
   has_answered: boolean | true
   is_from_bot: boolean | true
+
+  chat_id: string
+  remote_chat_id: string
+
+  answered_message_id?: string
 }
