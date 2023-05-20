9267f1b2eda5e31925dfae4ae9ee529cade54bd5
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-20 15:43:49 -0300

    [:white_check_mark:]  | fix: tests - api => messages events

--- next-gpt/test/app/api/messages/events/route.spec.ts
+++ next-gpt/test/app/api/messages/events/route.spec.ts
@@ -2,12 +2,32 @@
  * @jest-environment ./prisma/prisma-environment-jest
  */
 
-import { MESSAGES_EVENTS } from "@/config/routes"
+import { CHAT, MESSAGES_EVENTS } from "@/config/routes"
+import { Chat } from "@modules/chat/entity"
 import "isomorphic-fetch"
 
 describe("[API] - Message Events", () => {
+  let chat: Chat
+
+  it("Should create a chat", async () => {
+    const response = await fetch(`${CHAT}`, {
+      method: "POST",
+      body: JSON.stringify({ message: "Hello World" }),
+    })
+
+    const result = (await response.json()).chat
+
+    expect(response.status).toBe(200)
+    expect(result).toHaveProperty("id")
+
+    chat = result
+  })
+
   it("Should connect sucssessfully", async () => {
-    const response = await fetch(`${MESSAGES_EVENTS("1")}`)
-    expect(response.status).not.toBeNaN()
+    const id = chat.messages![chat.messages!.length - 1].id
+
+    const response = await fetch(`${MESSAGES_EVENTS(id!)}`)
+
+    expect(response.status).toBe(200)
   })
 })
