260aa16cfb1d3865cfe8861d0f3530d687b30f04
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-21 17:29:18 -0300

    [:sparkles:]  | create: middleware

---
+++ next-gpt/src/app/middleware.page.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  console.log("AAA")
+
+  return
+}
