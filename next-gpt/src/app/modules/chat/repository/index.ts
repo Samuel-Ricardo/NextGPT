0d3e9ff4829769312de229b534fc01ffc3e82213
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-22 10:02:33 -0300

    [:passport_control:]  | update: repository (chat)

--- next-gpt/src/app/modules/chat/repository/index.ts
+++ next-gpt/src/app/modules/chat/repository/index.ts
@@ -1,11 +1,16 @@
 import { Message } from "@modules/message/entity"
-import { IAddMessageDTO, ICreateChatDTO, IGetMessagesDTO } from "../DTO"
+import {
+  IAddMessageDTO,
+  ICreateChatDTO,
+  IGetMessagesDTO,
+  ISelectAllChatsDTO,
+} from "../DTO"
 import { Chat } from "../entity"
 import { IRepository } from "@repository/generic"
 
 export interface IChatRepository extends IRepository<Chat> {
   create(data: ICreateChatDTO): Promise<Chat>
-  selectAll(): Promise<Chat[]>
+  selectAll(data: ISelectAllChatsDTO): Promise<Chat[]>
   selectMessage(data: IGetMessagesDTO): Promise<Message[]>
   addMessageTo(data: IAddMessageDTO): Promise<Message>
 }
