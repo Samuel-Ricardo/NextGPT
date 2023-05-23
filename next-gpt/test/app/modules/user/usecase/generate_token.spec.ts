936c6497d1e7bd10d417c90e9e99dc4a5e854044
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-23 09:53:10 -0300

    [:white_check_mark:]  | create: unit test - usecase => generate token (user)

--- next-gpt/test/app/modules/user/usecase/generate_token.spec.ts
+++ next-gpt/test/app/modules/user/usecase/generate_token.spec.ts
@@ -1,5 +1,6 @@
 import { GenerateTokenUseCase } from "@/app/modules/user/usecase"
 import { ENV } from "@/config"
+import { GENERATE_TOKEN_DATA } from "@/config/const"
 import { decode } from "next-auth/jwt"
 
 describe("[USECASE] generate token => user", () => {
@@ -7,10 +8,16 @@ describe("[USECASE] generate token => user", () => {
   beforeEach(() => (generateToken = new GenerateTokenUseCase()))
 
   it("should return token when data is right", async () => {
-    const { token } = await generateToken.excute({
-      user: { sub: "123", name: "Samuel" },
-      secret: ENV.NEXT_AUTH.SECRET(),
-    })
+    const { token } = await generateToken.excute(GENERATE_TOKEN_DATA)
     expect(token).not.toBeNull()
+  })
+
+  it("should return a valid token", async () => {
+    const { token } = await generateToken.excute(GENERATE_TOKEN_DATA)
+    const decoded = await decode({ token, secret: GENERATE_TOKEN_DATA.secret })
+
+    expect(decoded).not.toBeNull()
+    expect(decoded?.sub).toBe(GENERATE_TOKEN_DATA.user.sub)
+    expect(decoded?.name).toBe(GENERATE_TOKEN_DATA.user.name)
   })
 })
