99c975f853493383452e83d5f398c446cb6a57c5
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-24 10:17:01 -0300

    [:sparkles:] | create: component - MarkDownRender

---
+++ next-gpt/src/app/components/markdown_render.tsx
@@ -0,0 +1,13 @@
+import { IMarkdownRenderProps } from "@Types"
+import { Markdown } from "@lib/markdown"
+
+export const MarkDownRender = (props: IMarkdownRenderProps) => {
+  const markdown = new Markdown(props.config)
+
+  return (
+    <div
+      className="relative w-[calc(100%-115px)] flex flex-col gap-1 transition duration-100 ease-linear break-words"
+      dangerouslySetInnerHTML={{ __html: markdown.format(props.content) }}
+    ></div>
+  )
+}
