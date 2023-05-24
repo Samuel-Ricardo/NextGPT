46066266d4c4e017d8f6ed40bce270fdaf79913c
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-24 10:28:30 -0300

    [:sparkles:] | update: component - ChatItem

--- next-gpt/src/app/components/chat/item.tsx
+++ next-gpt/src/app/components/chat/item.tsx
@@ -1,16 +1,16 @@
 import { UserIcon } from "@/app/assets/icons"
-import { LOGO } from "@/config/const"
+import { LOGO } from "@config/const"
 import { IChatItemProps } from "@Types"
-import { Loading } from ".."
+import { Loading, MarkDownRender } from ".."
 import Image from "next/image"
 
 export const ChatItem = ({ is_from_bot, content, loading }: IChatItemProps) => {
   const background = is_from_bot ? "bg-gray-800" : "bg-gray-600"
 
   return (
-    <li>
-      <div>
-        <div>
+    <li className={`w-full text-gray-100 ${background}`}>
+      <div className="flex-col">
+        <div className="md:max-w-2xl lg:max-w-xl xl:max-w-3xl py-6 m-auto flax flex-row items-start space-x-4">
           {is_from_bot ? (
             <Image
               src={LOGO.FULL_CYCLE}
@@ -19,14 +19,14 @@ export const ChatItem = ({ is_from_bot, content, loading }: IChatItemProps) => {
               height={30}
             />
           ) : (
-            <UserIcon className="" />
+            <UserIcon className="w-[30px] flex flex-col relative start"/>
           )}
 
-          <div />
+          <MarkDownRender content={content}/>
         </div>
 
         {loading && (
-          <div>
+          <div className="flex items-center justify-center pb-2">
             <Loading />
           </div>
         )}
