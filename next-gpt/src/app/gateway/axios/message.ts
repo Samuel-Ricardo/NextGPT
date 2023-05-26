ca3d9550bcfdfdb95b97f1faca86532160c6e418
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-25 23:24:29 -0300

    [:sparkles:] | create: gateway - message (axios)

--- next-gpt/src/app/gateway/axios/message.ts
+++ next-gpt/src/app/gateway/axios/message.ts
@@ -1,36 +1,38 @@
-import { IMessageGateway } from "@modules/message/gateway";
-import { GatewayGRCP } from "..";
-import { MESSAGES_EVENTS } from "@/config/routes";
+import { IMessageGateway } from "@modules/message/gateway"
+import { GatewayGRCP } from ".."
+import { MESSAGES_EVENTS } from "@config/routes"
 
-export class AxiosMessageGateway extends GatewayGRCP implements IMessageGateway {
- 
-  
+export class AxiosMessageGateway
+  extends GatewayGRCP
+  implements IMessageGateway
+{
   stream(path: string) {
-    console.log("INIT STREAM FOR:", path); 
-  
-    const eventSource = new EventSource(path);
- 
-    eventSource.onopen = (data) => console.log("[STREAM] - OPEN: ", {data})
-    
-    eventSource.onerror = (data) => { 
-      console.error("[STREAM] - ERROR: ", {data})
-      eventSource.close();
+    console.log("INIT STREAM FOR:", path)
+
+    const eventSource = new EventSource(path)
+
+    eventSource.onopen = (data) => console.log("[STREAM] - OPEN: ", { data })
+
+    eventSource.onerror = (data) => {
+      console.error("[STREAM] - ERROR: ", { data })
+      eventSource.close()
     }
 
-    eventSource.onmessage = data => console.log("[STREAM] - MESSAGE: ", {data})
+    eventSource.onmessage = (data) =>
+      console.log("[STREAM] - MESSAGE: ", { data })
 
     eventSource.addEventListener("end", (data) => {
-      console.log("[STREAM] - END", {data});
+      console.log("[STREAM] - END", { data })
       eventSource.close()
-    });
+    })
 
-    return  eventSource;
+    return eventSource
   }
 
   async writteMessage(id: string) {
-    const event = this.stream(MESSAGES_EVENTS(id));
-    const response = await this.get(MESSAGES_EVENTS(id));
-  
-    return {event, response};
+    const event = this.stream(MESSAGES_EVENTS(id))
+    const response = await this.get(MESSAGES_EVENTS(id))
+
+    return { event, response }
   }
 }
