8dbaa5ce1d645d5eb20988d4bc658345052decc1
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-25 23:39:19 -0300

    [:sparkles:] | update: gateway - chat (module)

--- next-gpt/src/app/modules/chat/gateway/default.ts
+++ next-gpt/src/app/modules/chat/gateway/default.ts
@@ -4,4 +4,5 @@ export interface IChatGateway extends IGatewayHTTP {
   create(message: string): Promise<any>
   getAll(): Promise<any>
   getMessages(id: string): Promise<any>
+  appendMessage(id: string, message: string): Promise<any>
 }
