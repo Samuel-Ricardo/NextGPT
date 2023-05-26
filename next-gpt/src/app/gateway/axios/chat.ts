e23a84d4bffc68027cb6cd6454c16655055137f1
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-26 00:02:28 -0300

    [:sparkles:] | update: gateway - chat (axios)

--- next-gpt/src/app/gateway/axios/chat.ts
+++ next-gpt/src/app/gateway/axios/chat.ts
@@ -1,6 +1,7 @@
 import { IChatGateway } from "@modules/chat/gateway"
 import { HTTPGateway } from ".."
 import { CHAT, CHAT_MESSAGES } from "@config/routes"
+import { IAppendMessagesBody } from "@/@types/api/chat"
 
 export class AxiosChatGateway extends HTTPGateway implements IChatGateway {
   async create(message: string) {
@@ -13,5 +14,9 @@ export class AxiosChatGateway extends HTTPGateway implements IChatGateway {
 
   async getMessages(id: string) {
     return await this.get(CHAT_MESSAGES(id))
+  }
+
+  async appendMessage(id: string, { message }: IAppendMessagesBody) {
+    return await this.post(CHAT_MESSAGES(id), { message })
   }
 }
