ac6f6c0986dbcfaeefc900dd3a9ace3aedb1c5bc
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-25 23:32:31 -0300

    [:sparkles:] | create: gateway - http (generic)

---
+++ next-gpt/src/app/gateway/generic/http.ts
@@ -0,0 +1,23 @@
+import { ENV } from "@config"
+import { IGatewayHTTP } from ".."
+import { Axios, AxiosRequestConfig } from "axios"
+
+export class HTTPGateway implements IGatewayHTTP {
+  constructor(
+    public readonly API_URL = ENV.API.URL(),
+    protected readonly client = new Axios()
+  ) {}
+
+  async get(path: string, config?: AxiosRequestConfig) {
+    return await this.client.get(`${this.API_URL}${path}`, config)
+  }
+  async post(path: string, body: any, config?: AxiosRequestConfig) {
+    return await this.client.post(`${this.API_URL}${path}`, body, config)
+  }
+  async put(path: string, body: any, config?: AxiosRequestConfig) {
+    return await this.client.put(`${this.API_URL}${path}`, body, config)
+  }
+  async delete(path: string, config?: AxiosRequestConfig) {
+    return await this.client.delete(`${this.API_URL}${path}`, config)
+  }
+}
