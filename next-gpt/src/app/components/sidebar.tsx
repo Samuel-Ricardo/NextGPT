1a400ca00db10702cd7213fcdc1984918a47df48
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-27 12:18:46 -0300

    [:sparkles:]  | create: component - Sidebar

--- next-gpt/src/app/components/sidebar.tsx
+++ next-gpt/src/app/components/sidebar.tsx
@@ -1,4 +1,4 @@
-export function Sidebar({children}: {children: React.ReactNode}) {
+export function Sidebar({ children }: { children: React.ReactNode }) {
   return (
     <div className="bg-gray-900 w-[300px] flex h-screen flex-col p-2">
       {children}
