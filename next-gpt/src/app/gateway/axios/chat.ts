82e3db4b835a3b8a0f71c79fce7d355eeb5c6b43
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-27 09:41:14 -0300

    [:sparkles:] | update: gateway - chat (axios)

--- next-gpt/src/app/gateway/axios/chat.ts
+++ next-gpt/src/app/gateway/axios/chat.ts
@@ -2,21 +2,28 @@ import { IChatGateway } from "@modules/chat/gateway"
 import { HTTPGateway } from ".."
 import { CHAT, CHAT_MESSAGES } from "@config/routes"
 import { IAppendMessagesBody } from "@/@types/api/chat"
+import { Chat } from "@modules/chat/entity"
+import { Message } from "@modules/message/entity"
+import { AxiosResponse } from "axios"
 
 export class AxiosChatGateway extends HTTPGateway implements IChatGateway {
   async create(message: string) {
-    return await this.post(CHAT, { message })
+    return (await this.post(CHAT, { message })) as AxiosResponse<{ chat: Chat }>
   }
 
   async getAll() {
-    return await this.get(CHAT)
+    return (await this.get(CHAT)) as AxiosResponse<{ chat: Chat[] }>
   }
 
   async getMessages(id: string) {
-    return await this.get(CHAT_MESSAGES(id))
+    return (await this.get(CHAT_MESSAGES(id))) as AxiosResponse<{
+      messages: Message[]
+    }>
   }
 
   async appendMessage(id: string, { message }: IAppendMessagesBody) {
-    return await this.post(CHAT_MESSAGES(id), { message })
+    return (await this.post(CHAT_MESSAGES(id), { message })) as AxiosResponse<{
+      message: Message
+    }>
   }
 }
