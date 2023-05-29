a077514adb4f6293246aa30b47e017d5d68a8bef
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-29 11:19:37 -0300

    [:sparkles:] | update: factory - life cycle (user)

--- next-gpt/src/app/modules/user/factory/life_cycle.ts
+++ next-gpt/src/app/modules/user/factory/life_cycle.ts
@@ -1,12 +1,21 @@
 import { UserLifeCycleController } from "../controller/lyfe_cycle"
 import { UserLifeCycleService } from "../service/life_cycle"
-import { AuthCheckUseCase, UserNotFoundUseCase } from "../usecase"
+import {
+  AuthCheckUseCase,
+  GenerateTokenUseCase,
+  UserNotFoundUseCase,
+} from "../usecase"
 
 export const lifeCycleFactory = () => {
   const auth_check = new AuthCheckUseCase()
   const not_found = new UserNotFoundUseCase()
+  const generate_token = new GenerateTokenUseCase()
 
-  const service = new UserLifeCycleService(auth_check, not_found)
+  const service = new UserLifeCycleService(
+    auth_check,
+    not_found,
+    generate_token
+  )
 
   const controller = new UserLifeCycleController(service)
 
