66919e3d0c3175f189a7b0d82b44fd6b63383655
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-29 09:13:50 -0300

    [:gear:]  | update: config - auth (next-auth)

--- next-gpt/src/app/api/auth/[...nextauth]/authConfig.ts
+++ next-gpt/src/app/api/auth/[...nextauth]/authConfig.ts
@@ -1,12 +1,15 @@
-import { ENV } from "@/config"
+import { ENV } from "@config"
+import { AuthOptions } from "next-auth"
 import keycloakProvider from "next-auth/providers/keycloak"
 
-export const authConfig = {
+export const authConfig: AuthOptions = {
   providers: [
     keycloakProvider({
       clientId: ENV.KEYCLOAK.CLIENT.ID() as string,
       clientSecret: ENV.KEYCLOAK.CLIENT.SECRET() as string,
-      issuer: ENV.KEYCLOAK.ISSUER(),
+      issuer: ENV.DOCKER.KEYCLOAK.ISSUER(),
     }),
   ],
+  secret: ENV.NEXT_AUTH.SECRET(),
+  debug: true,
 }
