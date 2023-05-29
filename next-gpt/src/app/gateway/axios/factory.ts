b8a71fbedf90f9af708599d09cf73908bedecb98
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-29 09:19:14 -0300

    [:sparkles:] | create: factory - gateway (axios)

---
+++ next-gpt/src/app/gateway/axios/factory.ts
@@ -0,0 +1,15 @@
+import { ENV } from "@config"
+import { AxiosChatGateway, AxiosMessageGateway, AxiosUserGateway } from "."
+import axios from "axios"
+
+export function axiosChatGatewayFactory() {
+  return new AxiosChatGateway(ENV.API.URL(), axios)
+}
+
+export function axiosMessageGatewayFactory() {
+  return new AxiosMessageGateway(ENV.API.URL(), axios)
+}
+
+export function axiosUserGatewayFactory() {
+  return new AxiosUserGateway(ENV.API.URL(), axios)
+}
