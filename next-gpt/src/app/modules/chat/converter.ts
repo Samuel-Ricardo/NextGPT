7fe912496d08168718d35609765bf05f37f98892
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-29 11:16:42 -0300

    [:bug:]  | fix: missing propertie - user_id (chat)

--- next-gpt/src/app/modules/chat/converter.ts
+++ next-gpt/src/app/modules/chat/converter.ts
@@ -5,6 +5,7 @@ import { Chat } from "./entity"
 export function chat(data: IChatConverterDTO): Chat {
   return Chat.from({
     id: data.id,
+    user_id: data.user_id!,
     messages: data.messages ? messages(data.messages) : [],
     remote_chat_id: data.remote_chat_id,
     created_at: data.created_at!,
