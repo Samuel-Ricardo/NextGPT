d9816fe507a91dfa8c5a44b814e7017159c2845d
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-29 09:14:35 -0300

    [:gear:]  | update: config - index

---
+++ next-gpt/src/config/server.ts
@@ -0,0 +1,7 @@
+// import NextCors from 'nextjs-cors'
+
+// export const CORS = async (req:Request, res:Response) => await NextCors(req, res, {
+//   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+//   origin: "*",
+//   optionsSuccessStatus:200,
+// })
