96d87b2acbd634c3f40eb0f542769edf7c0289ef
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-05-30 09:42:48 -0300

    [:bug:]  | fix: style - height & width (sidebar)

--- next-gpt/src/app/components/sidebar.tsx
+++ next-gpt/src/app/components/sidebar.tsx
@@ -1,6 +1,6 @@
 export function Sidebar({ children }: { children: React.ReactNode }) {
   return (
-    <div className="bg-gray-900 w-[300px] flex h-screen flex-col p-2">
+    <div className="bg-gray-900 w-[300px] flex h-full flex-col p-2">
       {children}
     </div>
   )
