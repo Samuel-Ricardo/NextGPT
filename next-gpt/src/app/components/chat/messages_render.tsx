d0aca446e60890f9d02c26ad91bbaffba91765ae
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-06-01 21:11:53 -0300

    [:sparkles:] | update: component - message render

--- next-gpt/src/app/components/chat/messages_render.tsx
+++ next-gpt/src/app/components/chat/messages_render.tsx
@@ -8,28 +8,35 @@ export function MessagesRender({
   loadingMessage,
   errorLoadingMessage,
 }: IMessagesRenderProps) {
+  
   return (
-    <ul
-      id={ELEMETNS.ID.CHATTING}
-      className="h-screen overflow-y-auto bg-gray-800"
-    >
-      {messages.map((message, key) => (
-        <ChatItem
-          key={key}
-          content={message.content}
-          is_from_bot={message.is_from_bot}
-        />
-      ))}
+    <div className="h-full w-full overflow-y-auto bg-gray-800 p-3">
+      <ul
+        id={ELEMETNS.ID.CHATTING}
+        className="h-full w-full overflow-y-auto bg-transparent rounded-xl"
+      >
+        {messages.map((message, key) => (
+          <ChatItem
+            key={key}
+            content={message.content}
+            is_from_bot={message.is_from_bot}
+          />
+        ))}
 
-      {loadingMessage && (
-        <ChatItem content={loadingMessage} is_from_bot={true} loading={true} />
-      )}
+        {loadingMessage && (
+          <ChatItem
+            content={loadingMessage}
+            is_from_bot={true}
+            loading={true}
+          />
+        )}
 
-      {errorLoadingMessage && (
-        <ChatItemError>{errorLoadingMessage}</ChatItemError>
-      )}
+        {errorLoadingMessage && (
+          <ChatItemError>{errorLoadingMessage}</ChatItemError>
+        )}
 
-      <li className="h-36 bg-gray-800"></li>
-    </ul>
+        <li className="h-36 bg-gray-800"></li>
+      </ul>
+    </div>
   )
 }
