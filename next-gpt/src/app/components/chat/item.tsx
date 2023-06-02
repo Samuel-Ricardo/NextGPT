fda73a4faa88fcb0f6b0d14182d84b4731e1db30
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-06-01 21:07:45 -0300

    [:sparkles:] | update: component - chat item

--- next-gpt/src/app/components/chat/item.tsx
+++ next-gpt/src/app/components/chat/item.tsx
@@ -5,10 +5,14 @@ import { Loading, MarkDownRender } from ".."
 import Image from "next/image"
 
 export const ChatItem = ({ is_from_bot, content, loading }: IChatItemProps) => {
-  const background = is_from_bot ? "bg-gray-800" : "bg-gray-600"
+  const background = is_from_bot ? "bg-gray-900" : "bg-gray-600"
+
+  console.log({ is_from_bot })
 
   return (
-    <li className={`w-full text-gray-100 ${background}`}>
+    <li
+      className={`min-w-max w-2/3 p-2 text-gray-100 ${background} rounded m-5 ml-auto mr-auto`}
+    >
       <div className="flex-col">
         <div className="md:max-w-2xl lg:max-w-xl xl:max-w-3xl py-6 m-auto flax flex-row items-start space-x-4">
           {is_from_bot ? (
@@ -17,9 +21,10 @@ export const ChatItem = ({ is_from_bot, content, loading }: IChatItemProps) => {
               alt="bot avatar image, full cycle logo"
               width={30}
               height={30}
+              className="mb-3 rounded"
             />
           ) : (
-            <UserIcon className="w-[30px] flex flex-col relative start" />
+            <UserIcon className="w-[30px] flex flex-col relative start mb-3 rounded" />
           )}
 
           <MarkDownRender content={content} />
