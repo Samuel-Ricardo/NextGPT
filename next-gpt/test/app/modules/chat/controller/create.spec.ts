5863f4a00527b1f9a771a9fa9ec737d4b9e7a74b
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-06-03 09:22:51 -0300

    [:white_check_mark:]  | fix: test suit - service => chat (module)

--- next-gpt/test/app/modules/chat/controller/create.spec.ts
+++ next-gpt/test/app/modules/chat/controller/create.spec.ts
@@ -7,8 +7,15 @@ import {
 } from "@/config/const"
 import { resetMocks } from "@test/utils/mock"
 import { NextRequest } from "next/server"
-import { request } from "http"
 import { ENV } from "@/config"
+import { IMessage } from "@/app/modules/message/model"
+
+jest.mock("../../../../../src/middlewarePack/index", () => ({
+  __esModule: true, // this property makes it work
+  AUTH: jest.fn(),
+}))
+
+import { AUTH } from "@/middlewarePack"
 
 describe("Controller => Chat", () => {
   jest.mock("../../../../../src/app/modules/chat/service/index")
@@ -30,57 +37,75 @@ describe("Controller => Chat", () => {
     request = new requestMock(ENV.API.URL()) as jest.Mocked<NextRequest>
   })
 
-  // it("Should create a Chat successfully", async () => {
-  //   jest.spyOn(service, "createChat").mockResolvedValue({
-  //     ...VALID_CHAT,
-  //     remote_chat_id: undefined,
-  //   })
+  it("Should create a Chat successfully", async () => {
+    jest.spyOn(service, "createChat").mockResolvedValue({
+      ...VALID_CHAT,
+      remote_chat_id: undefined,
+    })
 
-  //   await expect(controller.create(CREATE_CHAT_DTO)).resolves.toStrictEqual({
-  //     ...VALID_CHAT,
-  //     remote_chat_id: undefined,
-  //   })
+    await expect(controller.create(CREATE_CHAT_DTO)).resolves.toStrictEqual({
+      ...VALID_CHAT,
+      remote_chat_id: undefined,
+    })
 
-  //   expect(service.createChat).toBeCalledTimes(1)
-  // })
+    expect(service.createChat).toBeCalledTimes(1)
+  })
 
-  // it("Should select all Chats successfully", async () => {
-  //   jest.spyOn(service, "selectAllChats").mockResolvedValue([VALID_CHAT])
+  it("Should select all Chats successfully", async () => {
+    jest.spyOn(service, "selectAllChats").mockResolvedValue([VALID_CHAT])
 
-  //   await expect(
-  //     controller.selectAll({ user_id: CREATE_CHAT_DTO.user_id })
-  //   ).resolves.toStrictEqual([VALID_CHAT])
+    await expect(
+      controller.selectAll({ user_id: CREATE_CHAT_DTO.user_id })
+    ).resolves.toStrictEqual([VALID_CHAT])
 
-  //   expect(service.selectAllChats).toBeCalledTimes(1)
-  // })
+    expect(service.selectAllChats).toBeCalledTimes(1)
+  })
 
-  // it("Should select a Message successfully", async () => {
-  //   jest
-  //     .spyOn(service, "selectMessage")
-  //     .mockResolvedValue([VALID_IMESSAGE_WITHOUT_CHAT])
+  it("Should select a Message successfully", async () => {
+    jest
+      .spyOn(service, "selectMessages")
+      .mockResolvedValue([VALID_IMESSAGE_WITHOUT_CHAT])
 
-  //   await expect(
-  //     controller.select({ chat_id: VALID_CHAT.id!, token: { sub: "123" } })
-  //   ).resolves.toStrictEqual([VALID_IMESSAGE_WITHOUT_CHAT])
+    const result = await controller.selectMessages({
+      chat_id: VALID_CHAT.id!,
+      user_id: CREATE_CHAT_DTO.user_id,
+      token: { sub: CREATE_CHAT_DTO.user_id },
+    })
+    const data: { messages: IMessage[] } = await result.json()
 
-  //   expect(service.selectMessage).toBeCalledTimes(1)
-  // })
+    data.messages[0].created_at = new Date(data.messages[0].created_at)
 
-  // it("Should append a Message successfully", async () => {
-  //   jest
-  //     .spyOn(service, "appendMessage")
-  //     .mockResolvedValue(VALID_IMESSAGE_WITHOUT_CHAT)
+    expect(data).toEqual({ messages: [{ ...VALID_IMESSAGE_WITHOUT_CHAT }] })
+    expect(data.messages.length).toBe(1)
+    expect(data.messages[0]).toHaveProperty("id")
+    expect(service.selectMessages).toBeCalledTimes(1)
+  })
 
-  //   console.log({CYCLE: await controller.append({ chat_id: VALID_CHAT.id!, message: "Hello World", request })})
+  it("Should append a Message successfully", async () => {
+    jest
+      .spyOn(service, "appendMessage")
+      .mockResolvedValue(VALID_IMESSAGE_WITHOUT_CHAT)
 
-  //   await expect(
-  //     (await controller.append(
-  //       { chat_id: VALID_CHAT.id!, message: "Hello World", request }
-  //     )).json()
-  //   ).resolves.toStrictEqual(VALID_IMESSAGE_WITHOUT_CHAT)
+    AUTH.mockResolvedValue({ break: false, result: { sub: "123" } })
 
-  //   expect(service.appendMessage).toBeCalledTimes(1)
-  // })
+    const result = await controller.append({
+      chat_id: VALID_CHAT.id!,
+      message: "Hello World",
+      request,
+    })
+    const body = await result.json()
 
-  it("", () => expect(true).toBeTruthy())
+    body.message.created_at = new Date(body.message.created_at)
+
+    console.log({ CYCLE: body })
+
+    expect(AUTH).toBeCalledTimes(1)
+    expect(AUTH()).resolves.toStrictEqual({
+      break: false,
+      result: { sub: "123" },
+    })
+    expect(service.appendMessage).toBeCalledTimes(1)
+
+    expect(body).toEqual({ message: VALID_IMESSAGE_WITHOUT_CHAT })
+  })
 })
