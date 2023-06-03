2faf3495abb00866715c67f2ff7386b2719edd80
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-06-03 10:37:21 -0300

    [:white_check_mark:]  | creat: unit test - API => /chat (GET)

--- next-gpt/test/app/api/chat/route.spec.ts
+++ next-gpt/test/app/api/chat/route.spec.ts
@@ -10,7 +10,7 @@ import { headers } from "@/config/const"
 import "isomorphic-fetch"
 
 describe("[API] - route: /chat", () => {
-  it("[POST] - /chat | Should connect successfully but return unauthenticated error", async () => {
+  it("[POST] - /chat | Should connect successfully and return created chat", async () => {
     const CSRF = await (await fetch(ROUTES.BASE + "/auth/csrf")).json()
 
     const response = await fetch(ROUTES.CHAT, {
@@ -31,5 +31,22 @@ describe("[API] - route: /chat", () => {
 
     expect(created).toHaveProperty("chat")
     expect(created.chat).toHaveProperty("id")
+  })
+
+  it("[GET] - /chat | Should get all chats from logged user", async () => {
+    const response = await fetch(ROUTES.CHAT, { headers })
+    const data = await response.json()
+
+    expect(response.status).toBe(200)
+
+    expect(data).toHaveProperty("chat")
+    expect(data.chat).not.toBeNull()
+    expect(data.chat).toBeDefined()
+
+    expect(data.chat.length).not.toBeNaN()
+    expect(data.chat.length).toBeGreaterThan(-1)
+    expect(data.chat.length).not.toBeLessThan(0)
+
+    if (data.chat.length > 0) expect(data.chat[0]).toHaveProperty("id")
   })
 })
