41d13d550d41d97c66e0a55e7fff608253e51153
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-06-06 11:24:12 -0300

    [:white_check_mark:]  | create: test suit - E2E => Chatting (cypress)

---
+++ next-gpt/test/app/E2E/chat_flux.ts
@@ -0,0 +1,13 @@
+describe("Chatting Flux", () => {
+  const ID = {
+    GO_TO_CHAT_BUTTON: "go_to_chat_button",
+    HELLO_BUTTON: "hello_button",
+    MESSAGE: "message",
+    FORM: "form",
+    SUBMIT: "submit",
+    CHATTING: "chatting",
+    LOGOUT_BUTTON: "logout_button",
+  }
+
+  beforeEach(() => cy.visit("/"))
+})
