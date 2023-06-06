42e63b34e9c73f75c27ef4d1c0ff8e9646a528b1
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-06-06 11:19:57 -0300

    [:white_check_mark:]  | update: elements - ID

--- next-gpt/src/app/components/chat/sidebar.tsx
+++ next-gpt/src/app/components/chat/sidebar.tsx
@@ -2,6 +2,7 @@ import { IChatSidebarProps } from "@Types"
 import { Sidebar } from ".."
 import { LogoutIcon, PlusIcon } from "@/app/assets/icons"
 import { ChatList } from "./list"
+import { ELEMETNS } from "@/config/const"
 
 export function ChatSidebar({
   chats,
@@ -22,6 +23,7 @@ export function ChatSidebar({
       <ChatList chats={chats} onItemClick={onListItemClick} />
 
       <button
+        id={ELEMETNS.ID.LOGOUT_BUTTON}
         className="flex min-h-max min-w-max p-3  mt-1 gap-3 rounded hover:bg-gray-500/10 text-sm text-white transition-colors"
         onClick={onLogoutButtonClick}
       >
