6d5d0b69dde440146ebb36820d7afd3675af00c6
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-06-06 11:15:21 -0300

    [:white_check_mark:]  | create: test - E2E => Should logout sucsessfully (cypress)

--- next-gpt/test/app/E2E/initial_flux.cy.ts
+++ next-gpt/test/app/E2E/initial_flux.cy.ts
@@ -8,6 +8,7 @@ describe("Initial application flux", () => {
     FORM: "form",
     SUBMIT: "submit",
     CHATTING: "chatting",
+    LOGOUT_BUTTON: "logout_button",
   }
 
   beforeEach(() => cy.visit("/"))
@@ -43,5 +44,39 @@ describe("Initial application flux", () => {
     cy.get(`button[type=submit]`).click()
 
     cy.getCookie("next-auth.session-token").should("exist")
+  })
+
+  it("Should logout sucsessfully", () => {
+    cy.get(`#${ID.GO_TO_CHAT_BUTTON}`).click()
+
+    cy.url().should("include", "/login")
+
+    cy.origin("http://host.docker.internal:9000", () => {
+      cy.get(`#username`).type("admin")
+      cy.get(`#password`).type("admin")
+
+      //cy.getAllCookies().debug({log:true})
+      cy.getAllCookies().should("have.length.greaterThan", 0)
+
+      cy.getCookie("AUTH_SESSION_ID_LEGACY").should("exist")
+      cy.getCookie("KC_RESTART").should("exist")
+
+      cy.get(`input[type=submit]`).click()
+    })
+
+    cy.get(`button[type=submit]`).click()
+
+    cy.getCookie("next-auth.session-token").should("exist")
+
+    cy.get(`#${ID.GO_TO_CHAT_BUTTON}`).click()
+    cy.url().should("include", "/chat")
+
+    cy.get(`#${ID.LOGOUT_BUTTON}`).click()
+
+    cy.origin("http://host.docker.internal:9000", () => {
+      cy.get("input[type=submit]").click()
+    })
+
+    cy.getCookie("next-auth.session-token").should("not.exist")
   })
 })
