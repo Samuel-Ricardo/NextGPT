b50dfe72e1e0cbb61b935e2103e45e3fed1360c9
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-06-08 15:14:16 -0300

    [:white_check_mark:]  | add: test ID

--- next-gpt/src/app/components/loading.tsx
+++ next-gpt/src/app/components/loading.tsx
@@ -1,3 +1,8 @@
+import { ELEMETNS } from "@/config/const"
+
 export const Loading = () => (
-  <span className="animate-pulse animate-spin bg-gray-50 h-7 w-[8px] rounded"></span>
+  <span
+    id={ELEMETNS.ID.LOADING}
+    className="animate-pulse animate-spin bg-gray-50 h-7 w-[8px] rounded"
+  ></span>
 )
